a0c877fd9c9fa6895a01edf4c906d68ad57f9024
aland1013 <adam.land@gmail.com>
2020-11-05 20:32:48 -0500

    refactored summdiff.py

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -6,8 +6,8 @@ f(a) = f(c) - f(d) - f(b)
 """
 
 # q = set(range(1, 10))
-q = set(range(1, 200))
-# q = (1, 3, 4, 7, 12)
+# q = set(range(1, 200))
+q = (1, 3, 4, 7, 12)
 
 
 def f(x):
@@ -33,12 +33,14 @@ for solution in solution_set:
     sum_pairs = [k for k,v in sums.items() if v == solution]
     diff_pairs = [k for k,v in diffs.items() if v == solution]
     
-    for dp in diff_pairs:
-        c = dp[0]
-        d = dp[1]
-        for sp in sum_pairs:
-            a = sp[0]
-            b = sp[1]
+    for sp in sum_pairs:
+        a = sp[0]
+        b = sp[1]
+        
+        for dp in diff_pairs:
+            c = dp[0]
+            d = dp[1]
+    
             print(f'f({a}) + f({b}) = f({c}) - f({d})    {dict[a]} + {dict[b]} = {dict[c]} - {dict[d]}')
 
     
